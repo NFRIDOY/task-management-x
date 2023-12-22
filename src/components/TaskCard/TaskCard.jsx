27256f2d00f8eb3692ff2b970c89c29b6de97c8c
NF RIDOY <nfridoy@gmail.com>
2023-12-22 10:35:19 +0600

    task card gradient

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -20,25 +20,27 @@ export default function TaskCard({ postedTask, handleDelete }) {
 
   return (
     <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card w-96 h-96 shadow-xl    bg-gradient-to-b from-red-500 from-5%  to-red-300 to-90%">
         <div className="card-body">
-          <div className="flex justify-between">
-            <h2 className="card-title">{taskTitle}</h2>
-            <h2 className="card-title uppercase text-warning">{status}</h2>
+          <div className="flex justify-center w-full ">
+            <h2 className="card-title text-sm bg-red-700 text-white w-fit rounded-md py-1 px-2 ">{priority}</h2>
           </div>
-          <h2 className="card-title text-red-600">{priority}</h2>
-          <p>{description}</p>
+          <div className="flex justify-between border-b-2 pb-3">
+            <h2 className="card-title font-bold text-3xl">{taskTitle}</h2>
+            <h2 className="card-title uppercase text-info font-extrabold">{status}</h2>
+          </div>
+          <p className="text-white">{description}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">On Going</button>
+            <button className="btn btn-primary bg-red-700 border-none text-white hover:bg-red-800 ">On Going</button>
           </div>
         </div>
       </div>
-      <div>{_id} </div>
+      {/* <div>{_id} </div>
       <div>{email} </div>
       <div>{taskTitle} </div>
       <div>{description} </div>
-      <div>{priority} </div>
+      <div>{priority} </div> */}
     </div>
-    
+
   )
 }
